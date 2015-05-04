116eeb7f0e385004a9b7cc456a39af699105b383
Luigi Coniglio <luigi.coniglio@etu.unistra.fr>
2015-05-04 20:20:04 +0200

    Creation of the form, and little support for the submits

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+//For our small databases
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   res.render('index', { title: 'MiniTWR' });
+});
+
+/* POST tweet. */
+router.post('/', function(req, res, next) {
+	
 });
 
 module.exports = router;
